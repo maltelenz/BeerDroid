7c6abfbfca8e7513e35db8b12502d61ebc68969d
Malte Lenz <tyskis@gmail.com>
2010-07-11 12:39:01 +0200

    contact with ba

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,5 +1,29 @@
+require 'open-uri'
+require 'rubygems'
+require 'hpricot'
 class HomeController < ApplicationController
   def index
+    
   end
-
+  
+  def get_ba_by_id
+    url = "http://beeradvocate.com/beer/profile/" + params[:brewery] + "/" + params[:id]
+    page_content = open(url).read
+    page = Hpricot(page_content)
+    @beer_name = page.at("h1").inner_html
+    @rating = page.at("//td[@style='background:#FFFFFF;']/span[@class='BAscore_big']").inner_html
+    @style = page.at("//a[@href^='/beer/style/']/b").inner_html
+    @abv_element = page.at("//a[@href='/articles/518']").previous.inner_text
+    @abv = @abv_element[5..-3]
+    respond_to do |format|
+      format.html {
+        render :json => {
+          :beer_name => @beer_name,
+          :rating => @rating,
+          :style => @style,
+          :abv => @abv
+        }
+      }
+    end
+  end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -31,8 +31,9 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "home"
 
+  map.connect 'get_ba_by_id/:brewery/:id.:format', :controller => 'home', :action => 'get_ba_by_id'
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
