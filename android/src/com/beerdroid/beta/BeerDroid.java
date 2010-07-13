a7ac2497d9bc65d76a02747c3ed0bedcc1fe3309
Malte Lenz <tyskis@gmail.com>
2010-07-13 20:03:42 +0200

    android: initial empty project

---
+++ android/src/com/beerdroid/beta/BeerDroid.java
@@ -0,0 +1,13 @@
+package com.beerdroid.beta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeerDroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
